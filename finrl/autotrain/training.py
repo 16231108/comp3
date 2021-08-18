56a1b91e048b4ebeea7b2b6adf86e078fa32b8f4
unknown <jiadongwangbuaa@163.com>
2021-08-18 21:59:22 +0800

    k

--- finrl/autotrain/training.py
+++ finrl/autotrain/training.py
@@ -89,9 +89,9 @@ def train_one():
     ###################################################################################################a2c
     print("start training a2c model")
     model_a2c = agent.get_model("a2c")
-    trained_a2c = agent.train_lxc_model(
-        model=model_a2c, tb_log_name="a2c", total_timesteps=1000,lxcType=None,lxcName="lxc2"
-    )
+    # trained_a2c = agent.train_lxc_model(
+    #     model=model_a2c, tb_log_name="a2c", total_timesteps=1000,lxcType=None,lxcName="lxc2"
+    # )
     #print('trained_a2c is:', trained_a2c)
     all_model.append(trained_a2c)
 
@@ -109,9 +109,9 @@ def train_one():
     #################################################################ddpg
 
     model_ddpg = agent.get_model("ddpg")
-    trained_ddpg = agent.train_lxc_model(
-        model=model_ddpg, tb_log_name="ddpg", total_timesteps=1000,lxcType=None,lxcName="lxc1"
-    )
+    # trained_ddpg = agent.train_lxc_model(
+    #     model=model_ddpg, tb_log_name="ddpg", total_timesteps=1000,lxcType=None,lxcName="lxc1"
+    # )
     #print('trained_ddpg is:',trained_ddpg)
     all_model.append(trained_ddpg)
 
